3dc10a4f07f8b8e4576294f1b506b0ab398a1220
Charles Morisset <charles.morisset@newcastle.ac.ukmkdir>
2021-01-16 19:10:45 +0000

    Change reference to Michael.

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -11,7 +11,7 @@ const Main = (props) => (
   <>
     <Analytics />
     <ScrollToTop />
-    <Helmet titleTemplate="%s | Michael D'Angelo" defaultTitle="Michael D'Angelo">
+    <Helmet titleTemplate="%s | Charles Morisset" defaultTitle="Charles Morisset">
       {props.title && <title>{props.title}</title>}
       <meta name="description" content={props.description} />
     </Helmet>
